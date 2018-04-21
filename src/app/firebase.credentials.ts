b3599c1d45465fd345ff85ba9c5dc86205c16695
Kaai <kaai.cyre@gmail.com>
2018-04-21 19:30:16 +0100

    Added Firebase Credentials

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBPJ0hLtR6ceZrZxKxKFKVibjWLAMQxaV4",
+    authDomain: "shopping-list-be9cb.firebaseapp.com",
+    databaseURL: "https://shopping-list-be9cb.firebaseio.com",
+    projectId: "shopping-list-be9cb",
+    storageBucket: "shopping-list-be9cb.appspot.com",
+    messagingSenderId: "402156986053"
+};
